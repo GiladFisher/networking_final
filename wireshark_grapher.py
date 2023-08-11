0492a83a328fd1324a128eb896db787103848aee
GiladFisher <97436308+GiladFisher@users.noreply.github.com>
2023-08-11 17:19:28 +0200

    plotting

--- wireshark_grapher.py
+++ wireshark_grapher.py
@@ -1,7 +1,7 @@
 import pyshark
 import pandas as pd
 from datetime import timedelta
-
+import matplotlib.pyplot as plt
 
 def group_and_sum_within_interval(group):
     time_threshold = timedelta(seconds=2)
@@ -28,8 +28,8 @@ for packet in capture:
     }
     packet_data.append(packet_info)
     cnt += 1
-    if cnt > 27000:
-        break
+    # if cnt > 2700:
+    #     break
     # print(packet_info)
 
 df = pd.DataFrame(packet_data)
@@ -38,6 +38,21 @@ df.sort_values(by=['timestamp'], inplace=True)
 
 result = df.groupby(df['timestamp'].diff().gt(timedelta(seconds=1)).cumsum()) \
            .apply(group_and_sum_within_interval).reset_index(drop=True)
+result['total_size'] = result['total_size'].astype(float)
 print(result)
+plt.figure(figsize=(10, 6))  # Adjust the figure size if needed
+
+# Plot the data
+plt.plot(result['start_datetime'], result['total_size'], marker='o', linestyle='-', color='b')
+
+# Add labels and title
+plt.xlabel('Datetime')
+plt.ylabel('Total Size')
+plt.title('Total Size vs. Datetime')
+
+# Display the plot
+plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+plt.tight_layout()  # Adjust layout for better spacing
+plt.show()
 
 # print(df)
