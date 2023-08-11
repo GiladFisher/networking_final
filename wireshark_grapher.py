fb3cf5aa9bb4d9e138cfb5986a6107a918e622a9
GiladFisher <97436308+GiladFisher@users.noreply.github.com>
2023-08-11 14:21:19 +0200

    devided packets into groups by time

--- wireshark_grapher.py
+++ wireshark_grapher.py
@@ -1,14 +1,25 @@
 import pyshark
 import pandas as pd
+from datetime import timedelta
 
-#   Load the PCAP file
+
+def group_and_sum_within_interval(group):
+    time_threshold = timedelta(seconds=2)
+    within_interval = (group['timestamp'] - group['timestamp'].shift(1)) <= time_threshold
+    grouped = group[within_interval]
+    return pd.Series({
+        'start_datetime': grouped['timestamp'].iloc[0],
+        'total_size': grouped['length'].sum()
+    })
+
+
 pcap_file = 'filtered_recording_1.pcapng'
 capture = pyshark.FileCapture(pcap_file)
 
 packet_data = []
-
+cnt = 0
 for packet in capture:
-    print(packet.sniff_time)
+    # print(packet.sniff_time)
     packet_info = {
         'timestamp': packet.sniff_time,
         'length': packet.length,
@@ -16,8 +27,17 @@ for packet in capture:
         'destination_ip': packet.IPV6.dst if hasattr(packet, 'IPV6') else None,
     }
     packet_data.append(packet_info)
-    print(packet_info)
+    cnt += 1
+    if cnt > 27000:
+        break
+    # print(packet_info)
 
 df = pd.DataFrame(packet_data)
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+df.sort_values(by=['timestamp'], inplace=True)
 
-print(df)
+result = df.groupby(df['timestamp'].diff().gt(timedelta(seconds=1)).cumsum()) \
+           .apply(group_and_sum_within_interval).reset_index(drop=True)
+print(result)
+
+# print(df)
